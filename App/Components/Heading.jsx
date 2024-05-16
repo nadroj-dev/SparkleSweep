d0cfd78dc830abeb014acdecdc36e19aec95719d
nadroj-dev <150682254+nadroj-dev@users.noreply.github.com>
2024-05-16 23:23:21 +0800

    Heading.jsx

--- App/Components/Heading.jsx
+++ App/Components/Heading.jsx
@@ -1,27 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
-export default function Heading({text, isViewAll=false}) {
+export default function Heading({ text, isViewAll = false }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>
-        {text}
-      </Text>
-      {isViewAll&& <Text>View All</Text>}
+      <Text style={styles.heading}>{text}</Text>
+      {isViewAll && <Text>View All</Text>}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    heading: {
-      fontSize: 20,
-      fontFamily: 'outfit-medium',
-      marginBottom: 10
-    }
-})
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  heading: {
+    fontSize: 20,
+    fontFamily: 'outfit-medium',
+    marginBottom: 10,
+  },
+});
